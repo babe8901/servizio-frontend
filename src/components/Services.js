82a2f2bea2d2c8a581bb15a86b75bda2cebe1d50
Shubham Yadav <62867780+babe8901@users.noreply.github.com>
2023-05-11 19:14:17 +0000

    Services connected with API

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,153 +1,27 @@
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Cookies from "js-cookie";
 import { Link } from "react-router-dom";
 
-const services = [
-  {
-    id: 1,
-    name: "Haircut for men",
-    href: "/services/service-modal",
-    imageSrc: process.env.PUBLIC_URL + "/services/haircut.webp",
-    imageAlt: "Haircut for Men",
-    price: "₹199",
-  },
-  {
-    id: 2,
-    name: "Spa for Women",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/services/spa-for-women.webp",
-    imageAlt: "Spa for Women",
-    price: "₹399",
-  },
-  {
-    id: 3,
-    name: "Bathroom Cleaning",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/services/bathroom.webp",
-    imageAlt: "Bathroom Cleaning",
-    price: "₹499",
-  },
-  {
-    id: 4,
-    name: "Waxing for Women",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/services/waxing.webp",
-    imageAlt: "Waxing for Women",
-    price: "₹399",
-  },
-];
-
-const appliances = [
-  {
-    id: 1,
-    name: "Geyser",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/repair/geyser.webp",
-    imageAlt: "Geyser Repairing",
-    price: "₹499",
-  },
-  {
-    id: 2,
-    name: "Water Purifier",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/repair/water-purifier.webp",
-    imageAlt: "Water Purifier Repairing",
-    price: "₹299",
-  },
-  {
-    id: 3,
-    name: "Washing Machine",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/repair/washing-machine.webp",
-    imageAlt: "Washing Machine Repairing",
-    price: "₹799",
-  },
-  {
-    id: 4,
-    name: "Air Conditioner",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/repair/ac.webp",
-    imageAlt: "Air Conditioner Repairing",
-    price: "₹999",
-  },
-];
-
-const cleaning = [
-  {
-    id: 1,
-    name: "Bathroom and Kitchen",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/cleaning/b&k.webp",
-    imageAlt: "Bathroom and Kitchen Cleaning",
-    price: "₹499",
-  },
-  {
-    id: 2,
-    name: "Full Home",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/cleaning/home.webp",
-    imageAlt: "Full Home Cleaning",
-    price: "₹1999",
-  },
-  {
-    id: 3,
-    name: "Sofa & Carpet",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/cleaning/s&c.webp",
-    imageAlt: "Sofa & Carpet Cleaning",
-    price: "₹499",
-  },
-  {
-    id: 4,
-    name: "Pest Control",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/cleaning/pest.webp",
-    imageAlt: "Pest Control",
-    price: "₹999",
-  },
-];
-
-const others = [
-  {
-    id: 1,
-    name: "Carpenter",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/others/carpenter.webp",
-    imageAlt: "Carpenter",
-    price: "₹499",
-  },
-  {
-    id: 2,
-    name: "Electrician",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/others/electrician.webp",
-    imageAlt: "Electrician",
-    price: "₹499",
-  },
-  {
-    id: 3,
-    name: "Plumber",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/others/plumber.webp",
-    imageAlt: "Plumber",
-    price: "₹499",
-  },
-  {
-    id: 4,
-    name: "Painter",
-    href: "#",
-    imageSrc: process.env.PUBLIC_URL + "/others/painter.webp",
-    imageAlt: "Painter",
-    price: "₹499",
-  },
-];
-
 export default function Services() {
+  const url = "https://3875-49-32-249-5.ngrok-free.app/api/v1/services";
+  const [data, setData] = useState([]);
+
+  const fetchInfo = () => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then((d) => setData(d))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    fetchInfo();
+  }, []);
+
   return (
     <>
       <Header />
-      {/* <p>{this.state.crops}</p> */}
       <div className='bg-black'>
         <div className='max-w-2xl mx-auto py-5 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8'>
           <div className='pb-5'>
@@ -156,35 +30,47 @@ export default function Services() {
             </h2>
           </div>
           <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-            {services.map((service) => (
-              <div key={service.id} className='group relative'>
-                <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
-                  <img
-                    src={service.imageSrc}
-                    alt={service.imageAlt}
-                    className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                  />
-                </div>
-                <div className='mt-4 flex justify-between'>
-                  <div>
-                    <h3 className='text-sm text-white'>
-                      <Link to='/services/service-modal'>
-                        <a href={service.href}>
-                          <span
-                            aria-hidden='true'
-                            className='absolute inset-0'
-                          />
-                          {service.name}
-                        </a>
-                      </Link>
-                    </h3>
+            {data
+              .filter((service) => {
+                return service.type === "services";
+              })
+              .map((service) => (
+                <div key={service.id} className='group relative'>
+                  <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
+                    <img
+                      onClick={Cookies.set("service", JSON.stringify(service), {
+                        expires: 7,
+                      })}
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/" +
+                        service.type +
+                        service.imageSrc
+                      }
+                      alt={service.imageAlt}
+                      className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+                    />
                   </div>
-                  <p className='text-sm font-medium text-white'>
-                    {service.price}
-                  </p>
+                  <div className='mt-4 flex justify-between'>
+                    <div>
+                      <h3 className='text-sm text-white'>
+                        <Link to='/services/service-modal'>
+                          <a href={service.href}>
+                            <span
+                              aria-hidden='true'
+                              className='absolute inset-0'
+                            />
+                            {service.name}
+                          </a>
+                        </Link>
+                      </h3>
+                    </div>
+                    <p className='text-sm font-medium text-white'>
+                      {service.price}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
         <div className='max-w-2xl mx-auto py-5 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8'>
@@ -197,35 +83,51 @@ export default function Services() {
             </p>
           </div>
           <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-            {appliances.map((appliance) => (
-              <div key={appliance.id} className='group relative'>
-                <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
-                  <img
-                    src={appliance.imageSrc}
-                    alt={appliance.imageAlt}
-                    className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                  />
-                </div>
-                <div className='mt-4 flex justify-between'>
-                  <div>
-                    <h3 className='text-sm text-white'>
-                      <Link to='/services/service-modal'>
-                        <a href={appliance.href}>
-                          <span
-                            aria-hidden='true'
-                            className='absolute inset-0'
-                          />
-                          {appliance.name}
-                        </a>
-                      </Link>
-                    </h3>
+            {data
+              .filter((appliance) => {
+                return appliance.type === "repair";
+              })
+              .map((appliance) => (
+                <div key={appliance.id} className='group relative'>
+                  <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
+                    <img
+                      onClick={Cookies.set(
+                        "service",
+                        JSON.stringify(appliance),
+                        {
+                          expires: 7,
+                        }
+                      )}
+                      src={
+                        process.env.PUBLIC_URL +
+                        "/" +
+                        appliance.type +
+                        appliance.imageSrc
+                      }
+                      alt={appliance.imageAlt}
+                      className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+                    />
                   </div>
-                  <p className='text-sm font-medium text-white'>
-                    {appliance.price}
-                  </p>
+                  <div className='mt-4 flex justify-between'>
+                    <div>
+                      <h3 className='text-sm text-white'>
+                        <Link to='/services/service-modal'>
+                          <a href={appliance.href}>
+                            <span
+                              aria-hidden='true'
+                              className='absolute inset-0'
+                            />
+                            {appliance.name}
+                          </a>
+                        </Link>
+                      </h3>
+                    </div>
+                    <p className='text-sm font-medium text-white'>
+                      {appliance.price}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
         <div className='max-w-2xl mx-auto py-5 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8'>
@@ -235,33 +137,44 @@ export default function Services() {
             </h2>
           </div>
           <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-            {cleaning.map((item) => (
-              <div key={item.id} className='group relative'>
-                <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
-                  <img
-                    src={item.imageSrc}
-                    alt={item.imageAlt}
-                    className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                  />
-                </div>
-                <div className='mt-4 flex justify-between'>
-                  <div>
-                    <h3 className='text-sm text-white'>
-                      <Link to='/services/service-modal'>
-                        <a href={item.href}>
-                          <span
-                            aria-hidden='true'
-                            className='absolute inset-0'
-                          />
-                          {item.name}
-                        </a>
-                      </Link>
-                    </h3>
+            {data
+              .filter((item) => {
+                return item.type === "cleaning";
+              })
+              .map((item) => (
+                <div key={item.id} className='group relative'>
+                  <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
+                    <img
+                      onClick={Cookies.set("service", JSON.stringify(item), {
+                        expires: 7,
+                      })}
+                      src={
+                        process.env.PUBLIC_URL + "/" + item.type + item.imageSrc
+                      }
+                      alt={item.imageAlt}
+                      className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+                    />
                   </div>
-                  <p className='text-sm font-medium text-white'>{item.price}</p>
+                  <div className='mt-4 flex justify-between'>
+                    <div>
+                      <h3 className='text-sm text-white'>
+                        <Link to='/services/service-modal'>
+                          <a href={item.href}>
+                            <span
+                              aria-hidden='true'
+                              className='absolute inset-0'
+                            />
+                            {item.name}
+                          </a>
+                        </Link>
+                      </h3>
+                    </div>
+                    <p className='text-sm font-medium text-white'>
+                      {item.price}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
         <div className='max-w-2xl mx-auto py-5 px-4 sm:py-10 sm:px-6 lg:max-w-7xl lg:px-8'>
@@ -271,33 +184,44 @@ export default function Services() {
             </h2>
           </div>
           <div className='mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8'>
-            {others.map((item) => (
-              <div key={item.id} className='group relative'>
-                <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
-                  <img
-                    src={item.imageSrc}
-                    alt={item.imageAlt}
-                    className='w-full h-full object-center object-cover lg:w-full lg:h-full'
-                  />
-                </div>
-                <div className='mt-4 flex justify-between'>
-                  <div>
-                    <h3 className='text-sm text-white'>
-                      <Link to='/services/service-modal'>
-                        <a href={item.href}>
-                          <span
-                            aria-hidden='true'
-                            className='absolute inset-0'
-                          />
-                          {item.name}
-                        </a>
-                      </Link>
-                    </h3>
+            {data
+              .filter((item) => {
+                return item.type === "others";
+              })
+              .map((item) => (
+                <div key={item.id} className='group relative'>
+                  <div className='w-full min-h-40 bg-gray-200 rounded-md overflow-hidden group-hover:opacity-75 lg:h-40 lg:aspect-none'>
+                    <img
+                      onClick={Cookies.set("service", JSON.stringify(item), {
+                        expires: 7,
+                      })}
+                      src={
+                        process.env.PUBLIC_URL + "/" + item.type + item.imageSrc
+                      }
+                      alt={item.imageAlt}
+                      className='w-full h-full object-center object-cover lg:w-full lg:h-full'
+                    />
                   </div>
-                  <p className='text-sm font-medium text-white'>{item.price}</p>
+                  <div className='mt-4 flex justify-between'>
+                    <div>
+                      <h3 className='text-sm text-white'>
+                        <Link to='/services/service-modal'>
+                          <a href={item.href}>
+                            <span
+                              aria-hidden='true'
+                              className='absolute inset-0'
+                            />
+                            {item.name}
+                          </a>
+                        </Link>
+                      </h3>
+                    </div>
+                    <p className='text-sm font-medium text-white'>
+                      {item.price}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </div>
